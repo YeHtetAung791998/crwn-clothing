08fe1634096a10fc67d5d9685f9ebac595d0578a
mtm-yehtetaung <yehtetaung@metateammyanmar.com>
2023-10-20 00:30:06 +0630

    add routing

--- crwn-clothing/src/components/menu-item/menu-item.component.jsx
+++ crwn-clothing/src/components/menu-item/menu-item.component.jsx
@@ -1,18 +1,20 @@
 import React from "react";
 import './menu-item.styles.scss';
-const MenuItem = ({title,imageUrl,size}) => (
-    <div className={`${size} menu-item`}>
-        <div 
-        className="background-image"
-        style ={{
-        backgroundImage:`url(${imageUrl})`
-    }}>
+import { useNavigate } from "react-router-dom"; // Import useNavigate
+
+const MenuItem = ({ title, imageUrl, size, linkUrl }) => {
+  const navigate = useNavigate(); // Initialize the navigate function
+
+  return (
+    <div className={`${size} menu-item`} onClick={() => navigate(linkUrl)}>
+      <div className="background-image" style={{ backgroundImage: `url(${imageUrl})` }}>
         <div className='content'>
           <h1 className='title'>{title.toUpperCase()}</h1>
           <span className='subtitle'>SHOP NOW</span>
-              </div>
         </div>
+      </div>
     </div>
-);
+  );
+};
 
 export default MenuItem;
