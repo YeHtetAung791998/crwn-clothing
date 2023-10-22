43d4edb3fd0bac5f19d24e06391e6806354cb048
yehtetaung791998@gmail.com <yehtetaung777>
2023-10-22 19:25:52 +0630

    add firebase auth

--- crwn-clothing/src/App.js
+++ crwn-clothing/src/App.js
@@ -5,24 +5,47 @@ import './App.css';
 import HomePage from './pages/homepage/homepage.component';
 import ShopPage from './pages/shop/shop.component';
 import Header from './components/header/header.component';
+import SignInAndSignUpPage from './pages/sign-in-and-sign-up/sign-in-and-sign-up.component';
+import { auth } from './firebase/firebase.utils';
 
-
-function App() {
-  return (
-    <div>
+class App extends React.Component {
+  constructor() {
+     super();
+     this.state = {
+      currentUser: null,
+     };
+  }
+  unsubscribeFromAuth = null;
+  componentDidMount() {
+    this.unsubscribeFromAuth = auth.onAuthStateChanged( user => {
+      this.setState({ currentUser: user});
+      console.log(user);
+    })
+  }
       
-      <BrowserRouter>
-      <Header/>
-        <Routes>
-          <Route path='/' element={<HomePage />} />
-          <Route path='/shop' element={<ShopPage />} />
-        </Routes>
-      </BrowserRouter>
+componentWillUnmount(){
+    this.unsubscribeFromAuth();
+  }
 
+  render() {
+    return (
+      <div>
+        
+        <BrowserRouter>
+        <Header currentUser = {this.state.currentUser}/>
+          <Routes>
+            <Route path='/' element={<HomePage />} />
+            <Route path='/shop' element={<ShopPage />} />
+            <Route path='/signin' element={<SignInAndSignUpPage />} />
+          </Routes>
+        </BrowserRouter>
+  
+  
+  
+      </div>
+    );
+  }
 
-
-    </div>
-  );
 }
 
 export default App;
--- crwn-clothing/src/components/header/header.component.jsx
+++ crwn-clothing/src/components/header/header.component.jsx
@@ -1,9 +1,10 @@
 import React from "react";
 import { Link } from "react-router-dom";
 import './header.styles.scss';
+import { auth } from "../../firebase/firebase.utils";
 import {ReactComponent as Logo} from '../../assets/084 crown.svg';
 
-const Header = () => (
+const Header = ({currentUser}) => (
     <div className="header">
        <Link to="/" className="logo-container">
         <Logo className="logo"></Logo>
@@ -14,7 +15,14 @@ const Header = () => (
        </Link>     
        <Link to="/contact" className="option">
        CONTACT
-       </Link>    
+       </Link> 
+       {
+        currentUser ?
+        <div className="option" onClick={()=> auth.signOut()}>SIGN OUT</div>:
+        <Link className="option" to='/signin'>
+            SIGN IN
+        </Link>
+       }
        </div>
     </div>
 )
